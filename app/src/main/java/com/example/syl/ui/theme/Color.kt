6b99b4110eb905ce8da8b03a221c3247bbda3ea4
Iana Sanduta <iana.sandutsa@gmail.com>
2021-12-26 16:18:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/syl/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.syl.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val beige_01 = Color(0xFF8C7764)
+val beige_02 = Color(0xFFA68D77)
+val beige_03 = Color(0xFFBFB6AE)
+val beige_04 = Color(0xFF403B38)
+val beige_05 = Color(0xFFD9D3D0)
+val black = Color(0xFF000000)
+val white = Color(0xFFFFFFFF)
